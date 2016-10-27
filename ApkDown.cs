a73894e717dac7f333f361af80060c6319aa5615
Abdullah Gabous <slogutis_dp@mohmal.com>
2016-10-27 21:30:48 +0300

    Create ApkDown.cs

---
+++ ApkDown.cs
@@ -0,0 +1,20 @@
+// Coded by Slogutis (( iHack102 )) //
+
+Public Class ApkDown
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Apkdl.Show()
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Apkpure.Show()
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        MsgBox("SooN")
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        MsgBox("SooN")
+    End Sub
+End Class
